28be8a5b3af4922a67e3789f825fe10db3f764a8
Brandon Johnson <johnsbr99@gmail.com>
2023-09-02 09:20:07 -0600

    Added play method to createPet factory fx and created pet variables

--- js/script.js
+++ js/script.js
@@ -9,6 +9,29 @@ const createPet = function (name, species) {
     sleep: function () {
       console.log(`${this.name} needs a nap. Zzz...`);
       this.isTired = 1;
-    }
+    },
+    play: function (){
+      if(this.isTired===10) {
+        console.log("Too tired to play.");
+        this.sleep();
+      } else {
+        console.log(`Yay! ${this.name} is ready to play!`);
+        this.isTired+=1;
+      }
+    },
   };
+  return pet;
 };
+
+const sora=createPet("Sorra", "ferret");
+const clover=createPet("Clover", "rabbit");
+const baxter=createPet("Baxter", "hamster");
+const cleo=createPet("Cleo", "rat");
+const francine=createPet("Francine", "turtle");
+
+// console.log(sora, clover, baxter, cleo, francine);
+
+// clover.sleep();
+// baxter.play();
+
+// console.log(clover, baxter);
